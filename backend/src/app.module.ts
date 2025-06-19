0d3dfaedf6e10f60727cdaa1013b95f17024882e
mynameisleesiwon <seoil1221@gmail.com>
2025-06-19 11:11:30 +0900

    UserModule을 app.module.ts에 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { HelloModule } from './hello/hello.module';
 import { TodoModule } from './todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     HelloModule,
     TodoModule,
+    UserModule,
     // TypeORM 설정
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
